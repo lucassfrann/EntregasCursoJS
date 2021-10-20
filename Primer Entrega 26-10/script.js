cfb482bc3fdef0df8567b1b668b5292c220ebffa
Lucas Franceschelli <92877668+lucassfrann@users.noreply.github.com>
2021-10-20 15:34:07 -0300

    Adjunto entrega 1

---
+++ Primer Entrega 26-10/script.js
@@ -0,0 +1,11 @@
+
+let nombre = prompt('Hola, decinos tu nombre');
+console.log('¡Encantado de conocerte ' + nombre + '!');
+const numero1 = 12;
+let numero2 = parseInt(prompt('¿Que numero deseas sumarle a 12?'));
+let resultado = numero1 + numero2;
+console.log('Tu resultado final es ' + resultado);
+let texto1 = prompt('Escribe el nombre de la persona a la cual desea dedicarle un "Feliz cumpleaños"');
+const texto2 = 'Feliz cumpleaños'
+let textfinal = texto2 + " " + texto1
+alert(textfinal)
