434c6a6311d29bb31cdb1f1810da9198517857c6
Lucas Franceschelli <lucasfranceschelli@gmail.com>
2021-12-09 22:58:26 -0300

    asd

--- Proyecto final/Primer entrega parcial/Disenio pagina principal/js/producto.js
+++ Proyecto final/Primer entrega parcial/Disenio pagina principal/js/producto.js
@@ -28,29 +28,22 @@ function agregarProductoAlCarrito(producto) {
     Carrito.push(producto)
     let obJSON = JSON.stringify(Carrito)
     localStorage.setItem(`Productos`, obJSON)
-    // const ul = document.querySelector(".carrito-productos")
-    // let li = document.createElement("li")
-    // li.textContent = `Producto: ${producto.nombre} $ ${producto.precio}`
-    // ul.appendChild(li)
-    $(".carrito-productos").append(`<li> Prodcuto: ${producto.nombre} $${producto.precio}</li>`)
+    $(".carrito-productos").append(`<li class="productos-lista"> Producto: ${producto.nombre} $${producto.precio}</li>`)
     actualizarPrecioFinal()
 }
 
 function actualizarPrecioFinal() {
     let p = document.querySelector(".carrito-preciofinal")
     let precioparcial = Carrito.map(producto => producto.precio).reduce((a, b) => a + b, 0)
-    $(".carrito-preciofinal").append(`<p> Total: ${precioparcial} </p>`);
+    $(".carrito-preciofinal").text(`Total: ${precioparcial}`);
     return precioparcial
 }
 
 function vaciarCarrito() {
     localStorage.clear()
     Carrito.splice(0)
-    // let ul = document.querySelector(".carrito-productos")
-    // ul.innerHTML = ""
-    // let total = document.querySelector(".carrito-preciofinal")
-    $(".carrito-productos").innerHTML = ""
-    $(".carrito-preciofinal").innerHTML = ""
+    $(".carrito-productos .productos-lista").detach()
+    $(".carrito-preciofinal").text("")
 }
 
 function arrCarrito(precio) {
@@ -62,15 +55,9 @@ function arrCarrito(precio) {
 //--------------------------------------------------CONDICIONALES Y DEMAS------------------------------//
 
 
-let botones = document.getElementsByClassName("boton-agregar-producto");
 
-let tachocarrito = document.querySelector(".main-container-delete")
 
-let precios = document.getElementsByClassName("precio-producto");
-
-// let precios = $(".precio-producto")
-// let tachocarrito = $(".main-container-delete")
-// let botones = $(".boton-agregar-producto")
+let precios = $(".precio-producto");
 
 
 
@@ -78,11 +65,10 @@ for (let index = 0; index < precios.length; index++) {
     precios[index].innerHTML = `$${ListaProducto[index].precio}`
 }
 
-tachocarrito.addEventListener("click", vaciarCarrito)
 
-//$(".main-container-delete").on('click', vaciarCarrito)
 
-for (let i = 0; i < botones.length; i++) {
-    botones[i].addEventListener('click', () => agregarProductoAlCarrito(ListaProducto[i]));
-    // $(`.botones${ListaProducto[i]}`).on('click', () => agregarProductoAlCarrito(ListaProducto[i]))
+$(".main-container-delete").click(vaciarCarrito)
+
+for (let i = 0; i < ListaProducto.length; i++) {
+    $(`.boton-agregar-producto-${i}`).click(() => agregarProductoAlCarrito(ListaProducto[i]))
 }
