3d750c6477bd4c66a1fdbca820a4af688c9dbb18
Lucas Franceschelli <lucasfranceschelli@gmail.com>
2021-12-21 16:03:25 -0300

    a

--- Proyecto final/Primer entrega parcial/Disenio pagina principal/js/register.js
+++ Proyecto final/Primer entrega parcial/Disenio pagina principal/js/register.js
@@ -1,27 +1,27 @@
-const urljson2 = "data/datos.json";
+// const urljson2 = "data/datos.json";
 
-class datos {
-    constructor(inputnombre, inputapellido, inputemail, inputcontraseña) {
-        this.nombre = inputnombre;
-        this.apellido = inputapellido;
-        this.inputemail = inputemail;
-        this.contraseña = inputcontraseña;
-    }
-}
+// class datos {
+//     constructor(inputnombre, inputapellido, inputemail, inputcontraseña) {
+//         this.nombre = inputnombre;
+//         this.apellido = inputapellido;
+//         this.inputemail = inputemail;
+//         this.contraseña = inputcontraseña;
+//     }
+// }
 
-let inputnombre = $('name-input').val()
-let inputapellido = $('secondname-input').val()
-let inputemail = $('email-input').val()
-let inputcontraseña = $('password-input').val()
+// let inputnombre = $('name-input').val()
+// let inputapellido = $('secondname-input').val()
+// let inputemail = $('email-input').val()
+// let inputcontraseña = $('password-input').val()
 
-$("#button").click(function () {
+// $("#button").click(function () {
 
-    let info = []
-    info.push(datos)
+//     let info = []
+//     info.push(datos)
 
-    $.post(urljson2, info, function (data, status) {
-        if (status === "success") {
+//     $.post(urljson2, info, function (data, status) {
+//         if (status === "success") {
 
-        }
-    })
-})
+//         }
+//     })
+// })
