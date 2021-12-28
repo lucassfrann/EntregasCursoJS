09f6bab9faca43cfac0fb52d0d7805ad5b08e35a
Lucas Franceschelli <92877668+lucassfrann@users.noreply.github.com>
2021-12-28 19:29:46 -0300

    Add files via upload

--- Proyecto final/Primer entrega parcial/Disenio pagina principal/js/producto.js
+++ Proyecto final/Primer entrega parcial/Disenio pagina principal/js/producto.js
@@ -4,19 +4,25 @@ const ListaProducto = [{
         nombre: 'Polvo Matizador',
         stock: true,
         cantidadstock: 50,
-        precio: 950
+        precio: 950,
+        cantidadencarrito: 0,
+        id: 1
     },
     {
         nombre: 'Desamarillador',
         stock: true,
         cantidadstock: 50,
-        precio: 500
+        precio: 500,
+        cantidadencarrito: 0,
+        id: 2
     },
     {
         nombre: 'Cera Hairstyle',
         stock: true,
         cantidadstock: 50,
-        precio: 930
+        precio: 930,
+        cantidadencarrito: 0,
+        id: 3
     },
 ]
 
@@ -26,12 +32,17 @@ let Carrito = []
 
 function mostrarCarrito() {
     $(".carrito-li-p").slideDown();
+    $("#carrito-image").hide()
 }
 
 function ocultarCarrito() {
     $(".carrito-li-p").slideUp();
+    $("#carrito-image").show()
 }
 
+function resetearCarrito() {
+
+}
 
 function agregarProductoAlCarrito(producto) {
     Carrito.push(producto)
@@ -39,6 +50,7 @@ function agregarProductoAlCarrito(producto) {
     localStorage.setItem(`Productos`, obJSON)
     $(".carrito-productos").append(`<li class="productos-lista" style="background-color:white; text-decoration:none;" > Producto: ${producto.nombre} $${producto.precio}</li>`)
     $("#cantidad-productos").hide()
+    $("#finalizarpadre").show()
     actualizarPrecioFinal()
 }
 
@@ -57,10 +69,35 @@ function vaciarCarrito() {
     $("#cantidad-productos").show()
 }
 
-function arrCarrito(precio) {
-
+function finalizarCompra() {
+    if (finalizar == 0) {
+        $("#finalizar").hide()
+        $("#tacho").hide()
+        $(".container-carrito").append(`<p id="finalizar-text">Dejanos tu mail para contactarnos!</p>`)
+        $(".container-carrito").append(`<input id="finalizar-input" placeholder="E-mail"></input>`)
+        $(".container-carrito").append(`<p id="finalizar-boton">Enviar</p>`)
+        finalizar = 1
+    }
 }
 
+function validarEmail() {
+    let key = '965b2431f1a82289a54552e94b4afddd'
+    let email = $("finalizar-input").val()
+    console.log(email)
+    $.ajax({
+        method: GET,
+        url: 'http://apilayer.net/api/check?access_key=' + key + '&email=' + email,
+        dataType: 'jsonp',
+        success: function(json) {
+            console.log(json.format_valid);
+            console.log(json.smtp_check);
+            console.log(json.score);
+            alert('Datos enviados')
+        }
+    }
+    )
+}
+    
 
 
 //--------------------------------------------------CONDICIONALES Y DEMAS------------------------------//
@@ -70,6 +107,9 @@ function arrCarrito(precio) {
 
 let precios = $(".precio-producto");
 
+let cantproductos 
+
+let finalizar = 0
 
 
 for (let index = 0; index < precios.length; index++) {
@@ -80,8 +120,12 @@ $("#carrito-image").click(mostrarCarrito)
 
 $("#tacho").click(vaciarCarrito)
 
-$("#ojos").click(ocultarCarrito)
+$("#ocultar").click(ocultarCarrito)
+
+$("#finalizar").click(finalizarCompra)
+
+$("#finalizar-boton").click(validarEmail)
 
 for (let i = 0; i < ListaProducto.length; i++) {
-    $(`.boton-agregar-producto-${i}`).click(() => agregarProductoAlCarrito(ListaProducto[i]))
+    $(`#boton-agregar-producto-${i}`).click(() => agregarProductoAlCarrito(ListaProducto[i]))
 }
